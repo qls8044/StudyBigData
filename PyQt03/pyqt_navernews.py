82f651e2959dd21f27c20e2b837f2cf956f7ea28
qls8044 <gpqls9628@gmail.com>
2022-08-18 10:25:59 +0900

    pyqt 완료
    네이버 api 크롤링 -> csv 저장
    MySQL 저장 도전해보기

--- PyQt03/pyqt_navernews.py
+++ PyQt03/pyqt_navernews.py
@@ -10,13 +10,17 @@ from urllib.parse import quote
 import urllib.request
 import json
 import webbrowser
+import pandas as pd
 
 # 클래스 OOP
 class qTemplate(QWidget): # 생성자
-    
+    start = 1 # api 호출할 때 시작하는 데이터 번호
+    max_display = 100 # 한페이지에 나올 데이터 수
+    saveResult = [] # 저장할 때 담을 데이터(딕셔너리 리스트) -> DataFrame
+
     def __init__(self) -> None: 
         super().__init__()
-        uic.loadUi('./PyQt02/navernews.ui',self)
+        uic.loadUi('./PyQt03/navernews_2.ui',self)
         self.initUi()
     
     def initUi(self) -> None:
@@ -27,6 +31,29 @@ class qTemplate(QWidget): # 생성자
         self.btnSearch.clicked.connect(self.btnSearchClicked)
         self.txtSearch.returnPressed.connect(self.btnSearchClicked)
         self.tblResult.itemSelectionChanged.connect(self.tblResultSelected)
+        # 22.08.18 추가버튼 이벤트(시그널) 확장
+        self.btnNext.clicked.connect(self.btnNextClicked)
+        self.btnSave.clicked.connect(self.btnSaveClicked)
+
+    def btnNextClicked(self) -> None:
+        self.start = self.start + self.max_display
+        self.btnSearchClicked()
+
+    def btnSaveClicked(self) -> None:
+        if len(self.saveResult) > 0:
+            df = pd.DataFrame(self.saveResult)
+            df.to_csv(f'./PyQt03/{self.txtSearch.text()}_뉴스검색결과.csv', encoding='utf-8',index=True)
+
+        QMessageBox.information(self, '저장', '저장완료!')
+        # 저장 후 모든 변수 초기화
+        self.saveResult = []
+        self.start = 1
+        self.txtSearch.setText('')
+        self.lblStatus.setText('Data : ')
+        self.lblStatus2.setText('저장할데이터 > 0개')
+        self.tblResult.setRowCount(0)
+        self.btnNext.setEnabled(True)
+
 
     def tblResultSelected(self) -> None:
         selected = self.tblResult.currentRow() # 현재 선택된 열의 인덱스
@@ -38,14 +65,32 @@ class qTemplate(QWidget): # 생성자
         totalResult = []
         keyword = 'news'
         search_word = self.txtSearch.text()
-        display_count = 100
+
 
         # QMessageBox.information(self,'결과',search_word)
-        jsonResult = self.getNaverSearch(keyword,search_word, 1, display_count)
+        jsonResult = self.getNaverSearch(keyword,search_word, self.start,self.max_display)
         # print(jsonResult)
 
         for post in jsonResult['items']:
             totalResult.append(self.getPostData(post))
+
+        # saveResult 값 할당, lblStaus / 2 상태값을 표시
+        total = jsonResult['total']
+        curr = self.start + self.max_display - 1
+
+        self.lblStatus.setText(f'Data : {curr} / {total}')
+
+        # saveResult 변수에 저장할 데이터를 복사
+        for post in totalResult:
+            self.saveResult.append(post[0])
+
+        self.lblStatus2.setText(f'저장할데이터 > {len(self.saveResult)}개')
+
+        if curr >= 1000:
+            self.btnNext.setDisabled(True) # 다음 버튼 비활성화(네이버에서 1000건만 크롤링하는거 허락해줘서, 1000건 넘어가면 에러 안뜨게 처리함)
+        else:
+            self.btnNext.setEnabled(True) # 다음 버튼 활성화
+    
 
         # print(totalResult)
         self.makeTable(totalResult)
@@ -82,7 +127,7 @@ class qTemplate(QWidget): # 생성자
 
     def getPostData(self,post):
         temp = []
-        title = post['title']
+        title = self.strip_tag(post['title']) # 모든 곳에서 html 태그 제거
         description = post['description']
         originallink = post['originallink']
         link = post['link']
@@ -91,7 +136,8 @@ class qTemplate(QWidget): # 생성자
         temp.append({'title':title
                     ,'description':description
                     ,'originallink':originallink
-                    ,'link':link})
+                    ,'link':link
+                    ,'pubDate':pubDate}) # 220818 pubDate 빠진거 추가
         
         return temp
 
@@ -117,11 +163,7 @@ class qTemplate(QWidget): # 생성자
             return None
         else:
             return json.loads(ret)
-
-        
-
-
-    
+   
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
